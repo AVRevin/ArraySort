1f22e41a06c5d17f6c6d6f09d66f697478194a97
AVRevin <avrevin123@gmail.com>
2020-09-27 19:49:49 +0300

    first commit

---
+++ Array/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.IO;
+using System.Text;
+
+namespace ArraySort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region // загрузка файла
+
+            Console.WriteLine("Введите путь файла input.txt");
+            Console.WriteLine("Например C:\\Massiv\\input.txt");
+            string inputFileSource = Console.ReadLine();
+
+            StreamReader reader = new StreamReader(inputFileSource);
+
+            int inputArrayLenght = 0;
+            string inputArrayString = null;
+            int ouputMaxLenght = 0;
+            string outputArray = null;
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (i == 0) { inputArrayLenght = Convert.ToInt32(reader.ReadLine()); }
+                if (i == 1) { inputArrayString = reader.ReadLine(); }
+                if (i == 2) { ouputMaxLenght = Convert.ToInt32(reader.ReadLine()); }
+            }
+
+            reader.Close();
+
+            #endregion
+
+            #region//основной код
+
+            string[] stringarray = inputArrayString.Split(' ');
+            int[] intputarray = new int[inputArrayLenght];
+
+            for (int i = 0; i < inputArrayLenght; i++)
+            {
+                intputarray[i] = Convert.ToInt32(stringarray[i]);
+            }
+
+            Sorting.Sort(intputarray); // в сортировке используется Метод Шелла
+
+            for (int i = intputarray.Length - 1; i >= intputarray.Length - ouputMaxLenght; i--)
+            {
+                outputArray += Convert.ToString(intputarray[i]) + " ";
+            }
+
+            #endregion
+
+            #region  // сохранение файла
+
+            string fileName = inputFileSource.Remove(inputFileSource.Length - 9, 9) + "output.txt";
+
+            File.WriteAllText(fileName, outputArray);
+
+            Console.WriteLine("Сохранение прошло успешно");
+
+            # endregion 
+        }
+    }
+}
---
+++ Array/Sorting.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArraySort
+{
+    class Sorting
+    {
+        static public int[] Sort(int[] array)
+        {
+
+            int k = array.Length;
+            int temp;
+
+            while (k >= 1)
+            {
+                k = k / 2;
+
+                for (int i = 0; i < array.Length - k; i++)
+                {
+
+                    if (array[i] > array[i + k])
+                    {
+                        temp = array[i + k];
+                        array[i + k] = array[i];
+                        array[i] = temp;
+                    }
+
+                    for (int j = 0; j <= i; j++)
+                    {
+                        if (array[j] > array[j + k])
+                        {
+                            temp = array[j + k];
+                            array[j + k] = array[j];
+                            array[j] = temp;
+                        }
+                    }
+                }
+            }
+            return array;
+        }
+    }
+}
